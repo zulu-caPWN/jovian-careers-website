8f9b9540633acf617b287f992a9b5160bd22ab04
Zulu caPWN <zulu-caPWN@users.noreply.github.com>
2023-03-24 06:49:53 +0000

    pick back up at 1:13:24, https://www.youtube.com/watch?v=yBDHkveJUf4&t=4404s

--- app.py
+++ app.py
@@ -2,9 +2,34 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+  {'id':1,
+  'title': 'Data Analyst',
+  'location': 'Bengluru, India',
+  'salary': 'Rs. 10,00,000'
+  },
+  {'id':2,
+  'title': 'Data Dogger',
+  'location': 'Bengal, Tiger',
+  'salary': 'Rs. 10,00,000'
+  },
+  {'id':3,
+  'title': 'Data Diggler',
+  'location': 'Remote USA',
+  'salary': 'Rs. 10,00,000'
+  },
+  {'id':4,
+  'title': 'Data Docker',
+  'location': 'San Franciso, CA',
+  'salary': '$120,000'
+  },
+]
+
+
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs=JOBS,
+                          company_name="Jovan")
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
